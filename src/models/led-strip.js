5f6b0c3392300787d7b297c0a8688286695efc59
Eugene Malchenko <nijoelite@gmail.com>
2019-02-11 17:52:48 +0000

    Updated led-strip.js

--- src/models/led-strip.js
+++ src/models/led-strip.js
@@ -2,23 +2,24 @@ const raspi = require('raspi');
 const gpio = require('raspi-gpio');
 const pwm = require('raspi-soft-pwm');
 
-const RED_GPIO = 'GPIO6';
-const GREEN_GPIO = 'GPIO13';
+const RED_GPIO = 'GPIO13';
+const GREEN_GPIO = 'GPIO6';
 const BLUE_GPIO = 'GPIO5';
 
 
 class LedStrip {
 
     constructor(redPin, greenPin, bluePin) {
-        this.__redPin = new pwm.PWM(redPin || RED_GPIO);
-        this.__greenPin = new pwm.PWM(greenPin || GREEN_GPIO);
-        this.__bluePin = new pwm.PWM(bluePin || BLUE_GPIO);
+        this.__redPin = new pwm.SoftPWM(RED_GPIO);
+        this.__greenPin = new pwm.SoftPWM(GREEN_GPIO);
+        this.__bluePin = new pwm.SoftPWM(BLUE_GPIO);
     }
 
     setColor(r, g, b) {
-        this.__redPin.write(r / 255);
-        this.__greenPin(g / 255);
-        this.__bluePin(b / 255);
+	const minLvl = 0;
+        this.__redPin.write(Math.max(r, minLvl) / 255);
+        this.__greenPin.write(Math.max(g, minLvl) / 255);
+        this.__bluePin.write(Math.max(b, minLvl) / 255);
     }
 
 
