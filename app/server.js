9a5b96c15fbedc2d2fa109dd6264dfa397344834
Eugene Malchenko <nijoelite@gmail.com>
2019-03-02 03:23:05 +0300

    Update server

---
+++ app/server.js
@@ -0,0 +1,13 @@
+const config = require('./config.js');
+
+const express = require('express');
+const app = express();
+
+const router = require('./routes/index.js');
+
+app.use(express.static(__dirname + '/public'));
+app.use('/', router);
+
+app.listen(config.port, () => {
+    console.log(`Server is listening on port ${config.port}`);
+});
