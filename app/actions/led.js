380b306206f15653ae48f2ac4f7f653041f7111c
Eugene Malchenko <nijoelite@gmail.com>
2019-03-02 23:27:47 +0300

    Update led.js

--- app/actions/led.js
+++ app/actions/led.js
@@ -7,7 +7,7 @@ const manipulateStrip = (data) => {
     } else
     if (data.fade === true) {
         ledStrip.fade({red: +data.color1.red, green: +data.color1.green, blue: +data.color1.blue},
-            {red: +data.color2.red, green: +data.color2.green, blue: +data.color2.blue}, data.duration);
+            {red: +data.color2.red, green: +data.color2.green, blue: +data.color2.blue}, data.duration * 1000);
         console.log('Set Fade');
     }
 };
